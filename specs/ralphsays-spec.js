28f4022f94e65c4841ccd6a07e3021e1092bf658
Alister Scott <alister.scott@automattic.com>
2015-11-02 20:36:38 +1000

    Added better page objects and config

--- specs/ralphsays-spec.js
+++ specs/ralphsays-spec.js
@@ -1,30 +1,29 @@
 var assert = require('assert');
 var webdriver = require('selenium-webdriver');
 var test = require('selenium-webdriver/testing');
+var config = require('config');
 var RalphSaysPage = require('../lib/ralph-says-page.js');
 var driver;
 
-const mochaTimeOut = 30000; //ms
+const mochaTimeoutMS = config.get('mochaTimeoutMS');
 
 test.before(function() {
-    this.timeout(mochaTimeOut);
+    this.timeout(mochaTimeoutMS);
     driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 });
 
 test.describe('Ralph Says', function() {
-    this.timeout(mochaTimeOut);
+    this.timeout(mochaTimeoutMS);
 
     test.it('shows a quote container', function() {
-        var ralphSaysPage = new RalphSaysPage(driver);
-        ralphSaysPage.visit();
+        var ralphSaysPage = new RalphSaysPage(driver, true);
         ralphSaysPage.quoteContainerPresent().then(function(present) {
             assert.equal(present, true, "Quote container not displayed");
         });
     });
 
     test.it('shows a non-empty quote', function() {
-        var ralphSaysPage = new RalphSaysPage(driver);
-        ralphSaysPage.visit();
+        var ralphSaysPage = new RalphSaysPage(driver, true);
         ralphSaysPage.quoteTextDisplayed().then(function(text) {
             assert.notEqual(text, '', 'Quote is empty');
         });
