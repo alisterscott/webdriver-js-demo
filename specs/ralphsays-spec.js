aeb46240ff9d56e7ad3053e03c7736c45ab48766
Alister Scott <alister.scott@automattic.com>
2016-01-05 15:44:48 +1000

    Added ralph says as a page class

--- specs/ralphsays-spec.js
+++ specs/ralphsays-spec.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import webdriver from 'selenium-webdriver';
 import test from 'selenium-webdriver/testing';
 import config from 'config';
-import { ralphSays } from '../lib/pages.js';
+import RalphSaysPage from '../lib/ralph-says-page.js';
 
 let driver = null;
 
@@ -17,14 +17,14 @@ test.describe('Ralph Says', function() {
     this.timeout(mochaTimeoutMS);
 
     test.it('shows a quote container', function() {
-        var page = ralphSays(driver, true);
+        var page = new RalphSaysPage(driver, true);
         page.quoteContainerPresent().then(function(present) {
             assert.equal(present, true, "Quote container not displayed");
         });
     });
 
     test.it('shows a non-empty quote', function() {
-        var page = ralphSays(driver, true);
+        var page = new RalphSaysPage(driver, true);
         page.quoteTextDisplayed().then(function(text) {
             assert.notEqual(text, '', 'Quote is empty');
         });
