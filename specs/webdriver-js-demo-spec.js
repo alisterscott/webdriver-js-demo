03c2bf4ecc59b8ae66759c432b14b6ab7fbfcf76
Alister Scott <alister.scott@gmail.com>
2016-07-05 18:20:08 +1000

    Add alert test (#6)
    * Add alert test
    
    * Remove incorrect statements

--- specs/webdriver-js-demo-spec.js
+++ specs/webdriver-js-demo-spec.js
@@ -4,6 +4,7 @@ import test from 'selenium-webdriver/testing';
 import config from 'config';
 import WebDriverJsDemoPage from '../lib/webdriver-js-demo-page.js';
 import WebDriverJsErrorPage from '../lib/webdriver-js-error-page.js';
+import WebDriverJsLeavePage from '../lib/webdriver-js-leave-page.js';
 
 let driver = null;
 
@@ -22,9 +23,14 @@ test.describe( 'WebDriverJsDemo', function() {
 	test.it( 'can wait for an element to appear', function() {
 		let page = new WebDriverJsDemoPage( driver, true );
 		page.waitForChildElementToAppear();
-		page.childElementPresent().then( function( present ) {
+		page.childElementPresent().then( ( present ) => {
 			assert.equal( present, true, 'The child element is not present' );
 		} );
+	} );
+
+	test.it( 'can check for an alert when leaving the page', function() {
+		let page = new WebDriverJsLeavePage( driver, true );
+		page.navHome();
 	} );
 
 	test.it( 'can check for errors when there should be none', function() {
@@ -42,6 +48,14 @@ test.describe( 'WebDriverJsDemo', function() {
 	} );
 } );
 
-test.afterEach( () => driver.manage().deleteAllCookies() );
+test.afterEach( function() {
+	if ( this.currentTest.state === 'failed' ) {
+		driver.get( 'data:,' );
+		driver.switchTo().alert().then( ( alert ) => {
+			alert.accept();
+		}, () => {} );
+	}
+	driver.manage().deleteAllCookies()
+} );
 
 test.after( () => driver.quit() );
